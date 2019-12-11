dce805eed959bedcb234606d6c1950e07b257227
Blake Florence <bflorencedev@gmail.com>
2019-12-11 14:46:20 -0800

    MVP - Displaying Images and Text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 
 function App() {
+  const [data, setData] = useState("Data")
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=a6etMgp6umDLNczKiXvadyCNtnlYogALargYgT0p")
+      .then(response => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch(error => console.log("Error message", error));
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className="container">
+        <img src={data.url} alt="Nasa pictures of the galaxy"/>
+        <h1>{data.title}</h1>
+      <p>WOULD YOU LIKE TO KNOW MORE: {data.explanation}</p>
+      <h4>Image taken on {data.date}</h4>
+      </div>
     </div>
   );
 }
