239792f580e298890762435105e5ddc605e7c542
Blake Florence <bflorencedev@gmail.com>
2020-02-05 17:31:08 -0800

    playing with styled components 2.5

--- src/App.js
+++ src/App.js
@@ -2,46 +2,46 @@ import React from "react";
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import styled from "styled-components";
-import ".styled.js";
+import styled from "styled-components";
+// import ".styled.js";
 
-// const Button = styled.button`
-//   background: transparent;
-//   border-radius: 3px;
-//   border: 2px solid palevioletred;
-//   color: palevioletred;
-//   margin: 0 1em;
-//   padding: 0.25em 1em;
-//   `;
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  `;
 
-// const Img = styled.img`
-//   width: 100%;
-//   height: auto;
-// `;
+const Img = styled.img`
+  width: 100%;
+  height: auto;
+`;
 
-// const Container = styled.div`
-//   height: auto;
-//   width: 80%;
-//   padding: 2.5rem;
-// `;
+const Container = styled.div`
+  height: auto;
+  width: 80%;
+  padding: 2.5rem;
+`;
 
-// const NasaBackground = styled.div`
-//   height: auto;
-//   width: 100%;
-//   background-image: linear-gradient(
-//     to left bottom,
-//     #00449e,
-//     #e66465,
-//     #ff6f67,
-//     #ff9643,
-//     #2E8025,
-//     #f9f871
-//   );
-//   color: white;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `;
+const NasaBackground = styled.div`
+  height: auto;
+  width: 100%;
+  background-image: linear-gradient(
+    to left bottom,
+    #00449e,
+    #e66465,
+    #ff6f67,
+    #ff9643,
+    #2E8025,
+    #f9f871
+  );
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   const [data, setData] = useState("Data")
