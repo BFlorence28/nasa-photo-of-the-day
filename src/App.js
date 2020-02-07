8c3983ea30139c63df92e8c2352344b3d2bcb274
Blake Florence <bflorencedev@gmail.com>
2020-02-06 20:23:02 -0800

    mvp - adjusting styling

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
-// import ".styled.js";
 
-const Button = styled.button`
+// import "styled";
+// import { Button } from 'reactstrap';
+import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
+
+
+// const ButtonOne = styled.button`
+//   background: transparent;
+//   border-radius: 3px;
+//   border: 2px solid palevioletred;
+//   color: palevioletred;
+//   margin: 0 1em;
+//   margin-left: 350px;
+//   padding: 0.25em 1em;
+//   `;
+
+  const ButtonOne = styled.button`
   background: transparent;
   border-radius: 3px;
   border: 2px solid palevioletred;
   color: palevioletred;
   margin: 0 1em;
+  margin-left: 350px;
   padding: 0.25em 1em;
   `;
 
-const Img = styled.img`
-  width: 100%;
-  height: auto;
-`;
+// const Img = styled.img`
+//   width: 100%;
+//   height: auto;
+// `;
 
 const Container = styled.div`
   height: auto;
@@ -63,13 +78,40 @@ function App() {
   return (
     <NasaBackground>
       <Container>
+        {/* <Img> */}
         <img src={data.url} alt="Nasa pictures of the galaxy"/>
-        <h1>{data.title}</h1>
-        <Button>WOULD YOU LIKE TO KNOW MORE: </Button> <p>{data.explanation}</p>
+        {/* <h1>{data.title}</h1> */}
+        {/* <ButtonOne>WOULD YOU LIKE TO KNOW MORE: </ButtonOne> <p>{data.explanation}</p> */}
         <h4>Image taken on {data.date}</h4>
+        {/* </Img> */}
+
+  <div>
+    <Button color="palevioletred" id="toggler" 
+        style={{ 
+          marginBottom: '1rem',
+          marginLeft: '400px',
+          border: '2px solid palevioletred',
+          background: 'transparent',
+          borderRadius: '3px',
+          }}>
+      CLICK TO LEARN MORE!!!
+    </Button>
+    <UncontrolledCollapse toggler="#toggler">
+      <Card>
+        <CardBody>
+        <h1>{data.title}</h1>
+        <p>{data.explanation}</p>
+        {/* <h4>Image taken on {data.date}</h4> */}
+        </CardBody>
+      </Card>
+    </UncontrolledCollapse>
+  </div>
+
       </Container>
     </NasaBackground>
   );
 }
+
+
 
 export default App;
